a553160539b93e9aa01518ae63590a2b49083fb0
Stephen Mc Aleese <stephenmcaleese11@gmail.com>
2018-11-21 21:13:48 +0000

    Added multiple tests and the average time taken to python

--- insertion-sort-speed-test-python/insertion-sort-speed-test.py
+++ insertion-sort-speed-test-python/insertion-sort-speed-test.py
@@ -3,29 +3,43 @@
 # Note that this test takes about 20 seconds to run
 import time
 
+num_tests = 5
+
 a = []
 n = input()
-
 i = 0
 while i < 10000:
     a.append(n)
     n = input()
     i = i + 1
 
-def insertionSort():
+def insertionSort(arr):
     i = 1
-    while i < len(a):
-        v = a[i]
+    while i < len(arr):
+        v = arr[i]
         p = i
-        while 0 < p and v < a[p - 1]:
-            a[p] = a[p - 1]
+        while 0 < p and v < arr[p - 1]:
+            arr[p] = arr[p - 1]
             p = p - 1
-        a[p] = v
+        arr[p] = v
         i = i + 1
 
-start_time = time.time()
-insertionSort()
-time_taken = str(time.time() - start_time)
 
-print "Time taken to sort 10,000 random numbers using insertion sort: " + time_taken + " seconds"
+b = []
+total = 0
+x = 1
+while x <= num_tests:
+    b = a[:]  # passes by value instead of by reference which is the default
+    start_time = time.time()
+    insertionSort(b)
+    time_taken = time.time() - start_time
+    print "Test " + str(x) + \
+          ": Time taken to sort 10,000 random numbers using insertion sort: " + \
+          str(time_taken) + " seconds"
+    total = total + time_taken
+    x = x + 1
+
+average = total / num_tests
+print "Average time taken: " + str(average) + " seconds"
+
 
